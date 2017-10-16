9972d0d441c5423b02aeda9f152fbda8d39d18ad
zhanhb <zhanhb88@gmail.com>
2017-10-16 14:52:47 +0800

    Replace with RestController

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
-import org.springframework.stereotype.Controller;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = APPLICATION_JSON_VALUE)
     public Future<?> test() {
         return CompletableFuture.supplyAsync(() -> null);
     }
